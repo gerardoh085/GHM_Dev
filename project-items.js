ff6a7fe9ae9535cab06655b763bd7f527b40c810
gerardoh085 <gerardoh085@gmail.com>
2023-07-13 20:30:46 -0400

    project list to populate project items dynamically

---
+++ project-items.js
@@ -0,0 +1,43 @@
+
+var projects= [
+    {
+        name: "SkySkout",
+        image: "./images/SkySkout.png",
+        skills: ["JavaScript", "CSS", "HTML", "Cohere API", "Python", "React.js"],
+        github: "https://github.com/Ayon-Bhowmick/Hackrithmitic2"
+    },
+    {
+        name: "15 Puzzle",
+        image: "./images/FFTN_Puzzle.png",
+        skills: ["JavaScript", "CSS", "HTML"],
+        github: "https://github.com/gerardoh085/15-puzzle",
+    },
+    {
+        name: "Cookie Clicker Clone",
+        image: "./images/CookieClickerClone.png",
+        skills: ["React.js", "CSS", "HTML"],
+        github: "https://github.com/gerardoh085/CookieClickerClone",
+    },
+    {
+        name: "Tour Options",
+        image: "./images/Tour_Options.png",
+        skills: ["React.js","CSS","HTML"],
+        github: "https://github.com/gerardoh085/TourRecommendations",
+    },
+    {
+        name: "Calculator",
+        image: "./images/Calculator.png",
+        skills: ["JavaScript","CSS","HTML"],
+        github: "https://github.com/gerardoh085/Calculator",
+    },
+    {
+        name: "Rock Paper Scissors",
+        image: "./images/RockPaperScissors.png",
+        skills: ["JavaScript", "CSS", "HTML"],
+        github: "https://github.com/gerardoh085/RockPaperScissors",
+    }
+
+
+];
+
+export default projects;
