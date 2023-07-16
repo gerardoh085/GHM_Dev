e952686d29adf7b47b769acb8735bc3f0465dbf5
gerardoh085 <gerardoh085@gmail.com>
2023-07-15 23:37:11 -0400

    fixed video path

--- pages/scripts/robotics-items.js
+++ pages/scripts/robotics-items.js
@@ -1,27 +1,33 @@
+// https://gerardoh085.github.io/GHM_Dev/
 var robotics = [
     {
         name: "Programming the Freenove Robot",
-        video:"../../videos/CSE360_GHM_Lab2.mp4",
+        // video:"../../videos/CSE360_GHM_Lab2.mp4",
+        video:"./GHM_Dev/videos/CSE360_GHM_Lab2.mp4",
         description: "Connecting to Freenove Robot, getting familiar with documentation, and testing robots functionality."
     },
     {
         name: "Position Control with Ultrasound Part 1",
-        video:"../../videos/GHM_Lab3_pt1.mp4",
+        // video:"../../videos/GHM_Lab3_pt1.mp4",
+        video:"./GHM_Dev/videos/GHM_Lab3_pt1.mp4",
         description: "Utilized proportional controller to drive the robot to desired location through simulation and the actual robot"
     },
     {
         name: "Position Control with Ultrasound Part 2",
-        video:"../../videos/CSE360_GHM_Lab3_pt2.mp4",
+        // video:"../../videos/CSE360_GHM_Lab3_pt2.mp4",
+        video:"./GHM_Dev/videos/CSE360_GHM_Lab3_pt2.mp4",
         description:"Utilized proportional controller to drive the robot to desired location through simulation and the actual robot"
     },
     {
         name: "Search and Rescue Robot with OptiTrack",
-        video:"../../videos/CSE360_GHM_Mid.mp4",
+        // video:"../../videos/CSE360_GHM_Mid.mp4",
+        video:"./GHM_Dev/videos/CSE360_GHM_Mid.mp4",
         description: "Search and Rescue rubber duck by utilizing techniques such as the OptiTrack system and position control"
     },
     {
         name: "Planning Techniques: Shortest Paths",
-        video:"../../videos/GHM_Lab9_ShortestPath.mp4",
+        // video:"../../videos/GHM_Lab9_ShortestPath.mp4",
+        video:"./GHM_Dev/videos/GHM_Lab9_ShortestPath.mp4",
         description: "Used library networkx (BFS) to find the shortest path along with the OptiTrack system to identify Freenove Robot position."
     },
 ]
